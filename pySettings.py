d3ca3a407149817f3112279213efb140051a069f
Jordan Gumm <gumm1jn@cmich.edu>
2015-04-08 07:16:30 -0400

    updated pySettings for server settings

--- pySettings.py
+++ pySettings.py
@@ -1,2 +1,2 @@
-DATA_PATH = '/Users/lisa/Desktop/AutomaticClusterLabels/inData/'
-PRODUCTION_V = False
+DATA_PATH = '/home/gumm1jn/merge_columns_backend/uploader/AutomaticClusterLabels/inData/'
+PRODUCTION_V = True
