fa0202bce21200f1df2816ee2ee7eef4c5ad9fea
Lisa Gandy <lisagandy07@gmail.com>
2015-04-08 17:58:59 -0400

    small change in merge

--- pySettings.py
+++ pySettings.py
@@ -1,5 +1,5 @@
-#DATA_PATH = '/home/gumm1jn/merge_columns_backend/uploader/AutomaticClusterLabels/inData/'
-DATA_PATH = '/Users/lisa/Desktop/AutomaticClusterLabels/inData/'
-PRODUCTION_V = False
-#OUTPUT_PATH = '/var/www/uploads/files/'
-OUTPUT_PATH = '/Users/lisa/Desktop/'
+DATA_PATH = '/home/gumm1jn/merge_columns_backend/uploader/AutomaticClusterLabels/inData/'
+#DATA_PATH = '/Users/lisa/Desktop/AutomaticClusterLabels/inData/'
+PRODUCTION_V = True
+OUTPUT_PATH = '/var/www/uploads/files/'
+#OUTPUT_PATH = '/Users/lisa/Desktop/'
